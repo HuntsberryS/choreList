44203d5a08de1cbe4dc0b5c686ca18e58a1dfb21
HuntsberryS <huntsberry.shiloh@gmail.com>
2025-05-02 16:03:02 -0400

    style changes and added delete button functionality

--- index.js
+++ index.js
@@ -6,18 +6,21 @@ let chores = []
 let addChore = document.getElementById("addchore")
 let inputField = document.getElementById("inputfield")
 let choreList = document.getElementById("chorelist")
+let clearChores = document.getElementById("clearchores")
+let storedChores = JSON.parse(localStorage.getItem("chores"))
+
+
 // when arrow button clicked, run the add chore function 
 addChore.addEventListener("click", addToChores)
 
+if (storedChores) {
+    chores = storedChores
+    renderChoreList()
+}
 function addToChores() {
     chores.push(inputField.value)
     inputField.value =""
     localStorage.setItem("chores", JSON.stringify(chores))
-    console.log( localStorage.getItem("chores") )
-    
-    let storedChores = JSON.parse(localStorage.getItem("chores"))
-    console.log(storedChores)
-    
     renderChoreList()
 }
 
@@ -33,3 +36,14 @@ function renderChoreList () {
     choreList.innerHTML = choreItems
 }
 
+// when the x button is clicked, run the function to clear 
+clearChores.addEventListener("click", clearChoreList)
+
+function clearChoreList() {
+    //set the array back to empty 
+    chores = []
+    //call the function to discplay the list items, should be empty becuase the array is empty 
+    renderChoreList()
+    //clear the storage so old tasks wont show up constantly 
+    localStorage.clear()
+}
